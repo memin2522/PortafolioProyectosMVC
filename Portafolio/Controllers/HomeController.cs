f4569fffdcb4c252cc85b565e48eeaba3caabeee
Memo Reyes <sergio@yeltic.com>
2024-07-31 01:42:04 -0500

    Agregar archivos de proyecto.

---
+++ Portafolio/Controllers/HomeController.cs
@@ -0,0 +1,38 @@
+using Microsoft.AspNetCore.Mvc;
+using Portafolio.Models;
+using Portafolio.Services;
+using System.Diagnostics;
+
+namespace Portafolio.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+        private readonly IRepositorioProyectos repositorioProyectos;
+
+        public HomeController(ILogger<HomeController> logger, IRepositorioProyectos repositorioProyectos)
+        {
+            _logger = logger;
+            this.repositorioProyectos = repositorioProyectos;
+        }
+
+        public IActionResult Index()
+        {
+            _logger.LogInformation("This is a log message");
+            var proyects = repositorioProyectos.GetProyects().Take(3).ToList();
+            var modelo = new HomeIndexViewModel() { Proyectos  = proyects };
+            return View(modelo);
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ Portafolio/Services/RepositorioProyectos.cs
@@ -0,0 +1,46 @@
+﻿using Portafolio.Models;
+
+namespace Portafolio.Services
+{
+    public interface IRepositorioProyectos
+    {
+        List<Proyect> GetProyects();
+    }
+    public class RepositorioProyectos: IRepositorioProyectos
+    {
+        public List<Proyect> GetProyects()
+        {
+            return new List<Proyect>()
+            {
+                new Proyect
+                {
+                    Title = "Modulo del terapeuta",
+                    Description = "Modelo de servidor realizado en Flask",
+                    Link = "Yeltic.com",
+                    ImageUrl = "/images/servidor.png"
+                },
+                new Proyect
+                {
+                    Title = "Yeltic LeARn",
+                    Description = "Desarrollo del backend de la App sobre Firebase",
+                    Link = "Yeltic.com",
+                    ImageUrl = "/images/yeltic.png"
+                },
+                new Proyect
+                {
+                    Title = "Casilleros de carga Stereo Picnic",
+                    Description = "Desarrollo del backend local y sistema de comunicación con Arduino",
+                    Link = "Yeltic.com",
+                    ImageUrl = "/images/stereopicnic.png"
+                },
+                new Proyect
+                {
+                    Title = "Plugin Descarga Nativa",
+                    Description = "Creacion de un puglin nativo de android sobre Java para la descarga nativa de archivos en Unity",
+                    Link = "Yeltic.com",
+                    ImageUrl = "/images/android.png"
+                }
+            };
+        }
+    }
+}
